40f4720aab41c9ec3e00639f308f0cbf5ddc1b46
Adil Adomo <adiladomo@eduroam-085063.grenet.fr>
2023-03-10 11:13:57 +0100

    ajout identification google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Optional } from '@angular/core';
+import { Auth, authState, signInAnonymously, signOut, User, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { EMPTY, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { traceUntilFirst } from '@angular/fire/performance';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Miahoot';
+
+  private readonly userDisposable: Subscription|undefined;
+  public readonly user: Observable<User | null> = EMPTY;
+
+  showLoginButton = false;
+  showLogoutButton = false;
+
+  constructor(@Optional() private auth: Auth){
+    if (auth) {
+      this.user = authState(this.auth);
+      this.userDisposable = authState(this.auth).pipe(
+        traceUntilFirst('auth'),
+        map(u => !!u)
+      ).subscribe(isLoggedIn => {
+        this.showLoginButton = !isLoggedIn;
+        this.showLogoutButton = isLoggedIn;
+      });
+    }
+  }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    if (this.userDisposable) {
+      this.userDisposable.unsubscribe();
+    }
+  }
+
+  async login() {
+    return await signInWithPopup(this.auth, new GoogleAuthProvider());
+  }
+
+  async logout() {
+    return await signOut(this.auth);
+  }
+  
 }
