8c54e8d5b3192b521ddb43579eb331a810eef5a6
Adil Adomo <adiladomo@eduroam-085063.grenet.fr>
2023-03-10 10:26:19 +0100

    debut projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AccountConfigComponent } from './account-config/account-config.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    AccountConfigComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'miahoot-fb95b',
+    appId: '1:710221231684:web:9516304ce692ab31ccae11',
+    storageBucket: 'miahoot-fb95b.appspot.com',
+    apiKey: 'AIzaSyBZ5g5KKlzQyTxPIHC3GjSneNCYLyUcr0s',
+    authDomain: 'miahoot-fb95b.firebaseapp.com',
+    messagingSenderId: '710221231684',
+  },};
